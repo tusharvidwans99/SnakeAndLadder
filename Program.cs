f1b30052a7966a41510e4edfa3fbae8092685fee
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-05-25 13:18:44 +0530

    [Tushar Vidwans] Starting the Game as Player position is Zero

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to this problem simulates a Snake and Ladder Game.");
+            startGame.GameStarted();
         }
     }
 }
---
+++ startGame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class startGame
+    {
+        public static void GameStarted()
+        {
+
+            int Players = 1;
+            int Player1_Position = 0;
+            int[] LadderStart = { 4, 13, 33, 42, 50, 62, 74};
+            int[] SnakeStart = { 99, 89, 76, 66, 54, 43, 40, 27 };
+
+            Console.WriteLine($"Player Position: {Player1_Position}");
+        }
+    }
+}
