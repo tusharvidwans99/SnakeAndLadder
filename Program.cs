8a1da627290db7a1bb2f42d2e726b27c1ba56d7f
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-05-25 13:01:23 +0530

    [Tushar Vidwans] Adding Welcome Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SnakeAndLadder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to this problem simulates a Snake and Ladder Game.");
+        }
+    }
+}
